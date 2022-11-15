65dd214328c5c995bcdf3e731a70845b7e7ae53c
belugi <belugi@qq.com>
2022-11-15 20:13:05 +0800

    青桔更新

--- qj01.js
+++ qj01.js
@@ -1,13 +1,7 @@
 //new Env("青桔01")
-
-var https = require('follow-redirects').https;
-var fs = require('fs');
-
-var options = {
-    'method': 'POST',
-    'hostname': 'htwkop.xiaojukeji.com',
-    'path': '/gateway?api=prado.play.common.facade.component&apiVersion=1.0.0&appKey=h5appbcd0af7461691c1e30bcd61098f&appVersion=12.2.0&mobileType=Xiaomi&osType=2&osVersion=Android%252011&timestamp=1668309275104&ttid=h5&userRole=1&token=36dLqWUah2k4xVXAIWLXdAxaSIrVlVM_lSvojIUYqjwkzDuOwkAQBuG7VNyy_p6np2-zD-9CMkggIst3R-CoKvp2pgjyokUY0wk3ZiJylb83E97r6GvrQ157NmYhZMxKgPF15ptIY6intZSm0lo1fj_cRuw8bs_7z0ZUyf0w_ghvbc0aqcn4J_DqXcWl4hiXk70SOl4BAAD__w%253D%253D&userId=299072844604665&sign=8f8f5db0f8a8130836a573540273f68225112069&wsgsig=dd03-bj%252B3bcZExXhwA%252B2uN%252FvY%252Bnwap0avEJduMUCp3Wrfp0auBNqi8ARuNgO0ynhuBzPYKEGmM05GzmwZgQVU%252B9fP%252Biq8z0%252FOeJqT3qvZ%252BXOCzXAOep%252FOLqKz%252BnleQXq',
-    'headers': {
+var unirest = require('unirest');
+var req = unirest('POST', 'https://htwkop.xiaojukeji.com/gateway?api=prado.play.common.facade.component&apiVersion=1.0.0&appKey=h5appbcd0af7461691c1e30bcd61098f&appVersion=12.2.0&mobileType=Xiaomi&osType=2&osVersion=Android%2011&timestamp=1668309275104&ttid=h5&userRole=1&token=36dLqWUah2k4xVXAIWLXdAxaSIrVlVM_lSvojIUYqjwkzDuOwkAQBuG7VNyy_p6np2-zD-9CMkggIst3R-CoKvp2pgjyokUY0wk3ZiJylb83E97r6GvrQ157NmYhZMxKgPF15ptIY6intZSm0lo1fj_cRuw8bs_7z0ZUyf0w_ghvbc0aqcn4J_DqXcWl4hiXk70SOl4BAAD__w%3D%3D&userId=299072844604665&sign=8f8f5db0f8a8130836a573540273f68225112069&wsgsig=dd03-bj%2B3bcZExXhwA%2B2uN%2FvY%2Bnwap0avEJduMUCp3Wrfp0auBNqi8ARuNgO0ynhuBzPYKEGmM05GzmwZgQVU%2B9fP%2Biq8z0%2FOeJqT3qvZ%2BXOCzXAOep%2FOLqKz%2BnleQXq')
+    .headers({
         'Host': 'htwkop.xiaojukeji.com',
         'Connection': 'keep-alive',
         'Content-Length': '451',
@@ -21,29 +15,9 @@ var options = {
         'secdd-challenge': '3|v1.1.0||||||',
         'xpsid-root': '12cb2a5a1be6437da86a53be3ef7de02',
         'Referer': 'https://servicewechat.com/wx9e9b87595c41dbb7/369/page-frame.html'
-    },
-    'maxRedirects': 20
-};
-
-var req = https.request(options, function (res) {
-    var chunks = [];
-
-    res.on("data", function (chunk) {
-        chunks.push(chunk);
+    })
+    .send("%7B%22cityId%22%3A102%2C%22methodName%22%3A%22signIn%22%2C%22serviceCode%22%3A%22signInComponentService%22%2C%22serviceType%22%3A%22SIMPLE%22%2C%22entryFlag%22%3A%22APP_LETS%22%2C%22classifyCode%22%3A%22SIGN%22%2C%22scene%22%3A%22LLC_WELFARE_CENTER%22%2C%22bizId%22%3A363%2C%22channelId%22%3A3%2C%22miniappversion%22%3A%2212.2.0%22%2C%22klat%22%3A25.892251790364583%2C%22klnt%22%3A114.90707411024306%2C%22accuracy%22%3A15%2C%22hwId%22%3A%2210000%22%7D")
+    .end(function (res) {
+        if (res.error) throw new Error(res.error);
+        console.log(res.raw_body);
     });
-
-    res.on("end", function (chunk) {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-    });
-
-    res.on("error", function (error) {
-        console.error(error);
-    });
-});
-
-var postData =  "%7B%22cityId%22%3A102%2C%22methodName%22%3A%22signIn%22%2C%22serviceCode%22%3A%22signInComponentService%22%2C%22serviceType%22%3A%22SIMPLE%22%2C%22entryFlag%22%3A%22APP_LETS%22%2C%22classifyCode%22%3A%22SIGN%22%2C%22scene%22%3A%22LLC_WELFARE_CENTER%22%2C%22bizId%22%3A363%2C%22channelId%22%3A3%2C%22miniappversion%22%3A%2212.2.0%22%2C%22klat%22%3A25.892251790364583%2C%22klnt%22%3A114.90707411024306%2C%22accuracy%22%3A15%2C%22hwId%22%3A%2210000%22%7D";
-
-req.write(postData);
-
-req.end();
