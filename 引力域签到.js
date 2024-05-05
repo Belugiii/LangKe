42f5981ae20980523d7f2b0ac0670f41ba9e6cf4
LangKe <belugi@foxmail.com>
2024-05-05 23:26:54 +0800

    引力域

--- 引力域签到.js
+++ 引力域签到.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 let data = JSON.stringify({
-  "paramEncryptedStr": "Yzj5uym7bWBADiKYEIjkoA=="
+  "paramEncryptedStr": "+2AFPtt/uAwMKOPK0PVwlQ=="
 });
 
 let config = {
@@ -11,15 +11,15 @@ let config = {
     'Host': 'wxapi.uni.changan.com.cn', 
     'Connection': 'keep-alive', 
     'Content-Length': '48', 
-    'sign': 'FDF1CECF95D4BACF4371CBC9AB7F471B', 
-    'timestamp': '1711990872620', 
+    'sign': '5F2E60F4278BA555EEA43857DEFA2134', 
+    'timestamp': '1714922656844', 
     'content-type': 'application/json', 
-    'codeEncryptedStr': 'N+whpY2GIuzkvebY3W1WlM49J5RyNtDknlI9t9U82O+lHV23cVuYVsjOy91/hdQUzCDmCOfVsFibjkQXycMPvW8NeUIXgXq1JEYhUt99h1CpAecvco8WRR2AQnF64X1j+hZRRANEo9lWRwZ2018/Wg9sCGvdp+Cs6uoQVf4NHLo=', 
+    'codeEncryptedStr': 'N2oRDwlDII5hUJLOKoxtN4JoZwbxZPYKImtFv/eRDCfhOOsVe4IwyTQR7yrU1pneViRdxzR+g5aOp/CoYcXP9Rjtl6WqXjHzk5hdNCYK5dP34DuP5brfyJDdmhjo8CBCxlEbsTHIroe02yh890bDp0xp3w4VkWm4cUBVB/AdLZw=', 
     'token': 'user:token:min:16120154:A24723D06BB5330563BF28E7BD85D5F1', 
     'Accept-Encoding': 'gzip,compress,br,deflate', 
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.44(0x18002c2f) NetType/4G Language/zh_CN', 
-    'Referer': 'https://servicewechat.com/wxa85ccd055be3cfbc/212/page-frame.html', 
-    'Cookie': 'acw_tc=2f624a4e17119910647718872e0b7e9f0e4a96627f79a2f5d161863a49f7bd'
+    'Referer': 'https://servicewechat.com/wxa85ccd055be3cfbc/214/page-frame.html', 
+    'Cookie': 'acw_tc=2f624a4717149227240434862e5c74ba05096c875bfe9eae1295a4fc2e82ca'
   },
   data : data
 };
